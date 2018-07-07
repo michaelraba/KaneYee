5a1e8f65998f4867402dc66b62dbd1c62702fe11
mi <michael.raba@gmail.com>
2018-07-07 15:45:00 -0400

    fixed indices

--- yeeScheme.cc
+++ yeeScheme.cc
@@ -18,6 +18,7 @@ constexpr double pi() { return std::atan(1)*4; }
 class Matrix
 {
   friend class YeeScheme;
+  friend class Card;
 public:
   Matrix(int sizeX, int sizeY);
   int GetSizeX() const{return dx;}
@@ -39,15 +40,16 @@ private:
   int dx, dy;
 };
 
+// Matrix class constuctor. 
 Matrix::Matrix(int sizeX, int sizeY) : dx(sizeX), dy(sizeY)
-{ 
+{
   assert(sizeX > 0 && sizeY > 0);
   for(auto i=0; i<dx; ++i)
     {
       vector<double> temp;
       // add for i loop here..
       for(auto j =0; j < dy; ++j)
-        temp.push_back(0);
+        temp.push_back(j);
       data.push_back(temp);
     }
 }
@@ -78,11 +80,16 @@ void Matrix::Print() const
 
 
 class YeeScheme{
-friend class Matrix;
 public:
   YeeScheme();
-  YeeScheme(vector<Matrix> m);
-  void Print();
+  //thus we can write iterate.yee(field)
+  // we can create another constructor
+  // which would give the flexiblibity of
+  // not working with field..
+  // Orrr we can create another "field" with
+  // different sized matrices.
+
+  YeeScheme(vector<Matrix> m);   void Print();
   /* Obsolete methods for updating the interior
    * These are replaced by the single method YeeScheme::updateInterior()
 
@@ -94,7 +101,7 @@ public:
   enum PulseOptions { GAUSS_SOURCE, SINE_SOURCE /*...*/ };
   vector<Matrix> & updatePulse(int tStep, PulseOptions);
   enum BoundaryOptions { NEUMANN_BOUNDARY, DIRICHLET_BOUNDARY, TEST, BERENGER_PML /*...*/ };
-  enum ModeOptions { TM_MODE, TE_MODE, TEST_CASE /*...*/}; //maybe this enum belongs in main!
+  enum ModeOptions { TM_MODE, TE_MODE, TEST_CASE, SHITS /*...*/}; //maybe this enum belongs in main!
   vector<Matrix> & updateInterior(ModeOptions, vector<Matrix>::iterator it , int tStep);
   vector<Matrix> & iterateSolution(int tStep, ModeOptions);
   vector<Matrix> & updateBoundary(BoundaryOptions, vector<Matrix>::iterator it);
@@ -118,6 +125,93 @@ YeeScheme::YeeScheme(vector<Matrix> m)
 { 
   field=m; 
 }
+
+
+// PML class to constructor pml-objects
+// each object has m*n dimension
+// object acts as boundary
+// objects connect to interior region from left, top, etc.
+// PML-right needs to connect correctly to interior.. etc
+enum PMLType {left,right, top,bottom};
+class Card : public Matrix
+{
+ public:
+  enum mTyp{ INTERIOR, PML, NEUMANN /*...*/}; //maybe this enum belongs in main!
+  //  Card();
+  Card(int sizeX, int sizeY) ;
+  /* constructor for a matrix with:
+  ** dimension dx, dy
+  ** mType ie. PML, INTERIOR.. etc
+  * Thus we can create objects on the fly with a  specified
+  dimension etc.
+  * these objects operate on a field type ie {hx,hy,ez}
+   */
+  //this needs to operate on {hx,hy,dz}.. 
+
+  // update sthe interior..
+  Card(int sizeX, int sizeY, mTyp typ) ;
+  vector<Matrix> updateInterior;
+  // make method to format the card as a pml or interior or neumann boundary..!
+
+ private:
+  mTyp typ;
+
+
+  // the actal hx, hy,hz... data 
+  // this is the private data that each
+  // "card" has.
+  // we can push_back the actual field in main!
+  // ie. vector<Matrix> shit0,shit1,shit2,...,shit;
+  // shit0.push_back(8,16, PML) //has form of newest constructor 
+  // would make a 8 by 16 matrix with pml update cells.
+  vector<Matrix> field; 
+};
+
+
+Card::Card(int sizeX, int sizeY) : Matrix(sizeX,sizeY)
+// : dx(sizeX), dy(sizeY)
+{
+    assert(sizeX > 0 && sizeY > 0);
+    for(auto i=0; i<dx; ++i){
+      for(auto j=0; j<dx; ++j){
+      } //end j loop
+    } //end i loop
+      // for(auto i=0; i<dx; ++i)
+      //   {
+      //     vector<double> temp;
+      //     // add for i loop here..
+      //     for(auto j =0; j < dy; ++j)
+      //       temp.push_back(0);
+      //     data.push_back(temp);
+      //   }
+}
+
+// Card constructor with typ parameter
+// this should also ahve a TE or TM type option!
+Card::Card(int sizeX, int sizeY, mTyp typ) : Matrix(sizeX,sizeY)
+{
+
+  switch (typ)
+    {
+    case PML: // create a pml matrix. 
+      {
+  //  for(auto i=0; i<dx; ++i){
+  //    for(auto j=0; j<dx; ++j){
+  //      
+  //    } //end j loop
+  //  } //end i loop
+   for(auto i=0; i<dx; ++i)
+     {
+       vector<double> temp;
+       // add for i loop here..
+       for(auto j =0; j < dy; ++j)
+         temp.push_back(666);
+       data.push_back(temp);
+     } // end for loop
+      } // end pml case
+    } // end switch 
+      } //end constructor
+
 
 ostream &operator<<(ostream &out, const Matrix &m)
 {
@@ -274,29 +368,6 @@ vector<Matrix> & YeeScheme::updateBoundary(vector<YeeScheme::BoundaryOptions> b,
   //    }
   return field;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 vector<Matrix> & YeeScheme::updatePulse(int tStep, PulseOptions p)
 {
@@ -543,104 +614,189 @@ vector<Matrix> &  YeeScheme::iterateSolution(int tStep, YeeScheme::ModeOptions m
 
 
 
-//int main(int argc, char* argv[])
-//{
-//  Matrix foo(2,3);
-//  foo.Print(); // this prints
-//
-//  cout<<"Print Bar:\n";
-//  Matrix bar(3,2);
-//   bar.Print(); //this does not print
-//   bar.makeIdentity();
-//   bar.Print(); //this does not print
-//  
-//  return 0;
-//}
 
 int main(int argc, char* argv[])
 {
+  Card myPml(8,4);
+  Card poopy(8,4,Card::PML);
+  cout<<"myPml is:\n"<<myPml;
 
-  enum ModeOptions { TM_MODE, TE_MODE/*...*/ }; //keep this enim in YeeScheme (for now..)
-    int l,n,i,j,ic,jc,nsteps,npml;
-    float ddx,dt,T,epsz,pi,epsilon,sigma,eaf;
-    float t0,spread,pulse;
-    T  =0;
-    if (argc == 1 )
-      nsteps = 2;
-    else
-      {
-        istringstream ss(argv[1]);
-        int x;
-        if (!(ss >> x))
-          cerr << "Invalid number " << argv[1] << '\n';
-        nsteps = x;}
-    ic = IE/2;
-    jc = JE/2;
-    ddx = 0.01; // spatial step
-    dt = ddx/6e8; // yee cell size // why...because CFL condition..t scales different from x..
-    epsz=8.8e-12;
-    pi=3.14159;
+  // now how to feed fields into card? no, it is a private
+  // field for each card.
 
-    cout<<"^^^^^^^^^^^^^Begin Iterates:\n";
-    ModeOptions m = TE_MODE;
-    YeeScheme::BoundaryOptions b = YeeScheme::BERENGER_PML;
-    
-    switch (m)
-      {
-      case TM_MODE:
-        {
-          Matrix ga(IE,JE),dz(IE,JE),ez(IE,JE) ;
-          Matrix hx(IE,JE),hy(IE,JE);
-          ga.makeIdentity();
-          int dSize = IE;
-          //where is ex, ey fields? haha! And Hz, lol.
-          vector<Matrix> field = {dz,hx,hy,ez,ga};
-          //  vector<Matrix> Efield = {ex,ey,ez};
-          YeeScheme yee(field); // how would i call yee without field?
-          // i maybe do not create the yee object in main. Ok.
-          for(n=1; n<nsteps; ++n)
-            {
-                      cout<< "*****Iteration step:"<< n<<"******" << endl;
-              T= T+1;
-              yee.iterateSolution(T, YeeScheme::TM_MODE);
-              //field[0].PrintToFile(T);
-            }
-        } //end tm-mode case
+  // maybe rename Card to Region
+  // maybe i should create the field
+  // in a cases environment still
+    Card s0(8,16, Card::PML);
+    Card s1(8,16, Card::PML);
+    Card s2(8,16, Card::PML);
+    Card s3(8,16, Card::PML);
+    Card s4(8,16, Card::PML);
+    Card s5(8,16, Card::PML);
+    Card s6(8,16, Card::PML);
+    Card s7(8,16, Card::PML);
+    Card s8(8,16, Card::PML);
+    //Prototyp: needs to have form:
+    //Card s8(8,16, Card::PML, TM);
+    //           - or -           //
+    //Card s8(8,16, Card::PML(TM, connectsWithOrientationLeft) );
+    //Card s8(8,16, Card::PML(TM, orientation(<)) );
 
-      case TE_MODE:
-        {
-          switch(b)
-            {
-          case YeeScheme::BERENGER_PML:
-            {
-              cout<<"*******hi******";
-              int cellSize=4;
-              int IEx=IE+cellSize;
-              int JEx=JE+cellSize;
-              Matrix hzx(IEx,JEx),hzy(IEx,JEx);
-              Matrix experiment(IE,cellSize);
-              experiment.makeIdentity();
-              cout << "my experiment:\n"<<experiment;
-            } //end berenger_pml case
-            } //end boundary switch
-          //
-          //          Matrix ga(IE,JE); //auxillary matrices..
-          //          Matrix ex(IE,JE),ey(IE,JE),hz(IE,JE);
-          //          ga.makeIdentity();
-          //          int dSize = IE;
-          //          //where is ex, ey fields? haha! And Hz, lol.
-          //          vector<Matrix> field = {ex,ey,hz,ga};
-          //          YeeScheme yee(field); // how would i call yee without field?
-          //          // i maybe do not create the yee object in main. Ok.
-          //          for(n=1; n<nsteps; ++n)
-          //            {
-          //                      cout<< "*****Iteration step:"<< n<<"******" << endl;
-          //              T= T+1;
-          //              yee.iterateSolution(T, YeeScheme::TE_MODE);
-          //              //field[0].PrintToFile(T);
-          //            }
-        } //end te-mode case
-      }
+
+
+    // corner piece -> just one parameter.
+    //Card s8(8,16, Card::PML(TM) );
+
+    //interior isotropic region--> 1 parameter 
+    //Card s8(8,16, Card::vacuum(TM) );
+
+
+  //  enum ModeOptions { TM_MODE, TE_MODE,SHITS/*...*/ }; //keep this enim in YeeScheme (for now..)
+  //    int l,n,i,j,ic,jc,nsteps,npml;
+  //    float ddx,dt,T,epsz,pi,epsilon,sigma,eaf;
+  //    float t0,spread,pulse;
+  //    T  =0;
+  //    if (argc == 1 )
+  //      nsteps = 2;
+  //    else
+  //      {
+  //        istringstream ss(argv[1]);
+  //        int x;
+  //        if (!(ss >> x))
+  //          cerr << "Invalid number " << argv[1] << '\n';
+  //        nsteps = x;}
+  //    ic = IE/2;
+  //    jc = JE/2;
+  //    ddx = 0.01; // spatial step
+  //    dt = ddx/6e8; // yee cell size // why...because CFL condition..t scales different from x..
+  //    epsz=8.8e-12;
+  //    pi=3.14159;
+  //
+  //    cout<<"^^^^^^^^^^^^^Begin Iterates:\n";
+  //    ModeOptions m = TE_MODE;
+  //    YeeScheme::BoundaryOptions b = YeeScheme::BERENGER_PML;
+  //    
+  //    switch (m)
+  //      {
+  //      case TM_MODE:
+  //        {
+  //          Matrix ga(IE,JE),dz(IE,JE),ez(IE,JE) ;
+  //          Matrix hx(IE,JE),hy(IE,JE);
+  //          ga.makeIdentity();
+  //          int dSize = IE;
+  //          //where is ex, ey fields? haha! And Hz, lol.
+  //          vector<Matrix> field = {dz,hx,hy,ez,ga};
+  //          //  vector<Matrix> Efield = {ex,ey,ez};
+  //          YeeScheme yee(field); 
+  //
+  //
+  //          for(n=1; n<nsteps; ++n)
+  //            {
+  //                      cout<< "*****Iteration step:"<< n<<"******" << endl;
+  //              T= T+1;
+  //              yee.iterateSolution(T, YeeScheme::TM_MODE);
+  //              //field[0].PrintToFile(T);
+  //            }
+  //        } //end tm-mode case
+  //
+  //      case TE_MODE:
+  //        {
+  //          switch(b)
+  //            {
+  //          case YeeScheme::BERENGER_PML:
+  //            {
+  //              cout<<"*******hi******";
+  //              int cellSize=4;
+  //              int IEx=IE+cellSize;
+  //              int JEx=JE+cellSize;
+  //              Matrix hzx(IEx,JEx),hzy(IEx,JEx);
+  //              Matrix experiment(IE,cellSize);
+  //              experiment.makeIdentity();
+  //              cout << "my experiment:\n"<<experiment;
+  //            } //end berenger_pml case
+  //            } //end boundary switch
+  //          //
+  //          //          Matrix ga(IE,JE); //auxillary matrices..
+  //          //          Matrix ex(IE,JE),ey(IE,JE),hz(IE,JE);
+  //          //          ga.makeIdentity();
+  //          //          int dSize = IE;
+  //          //          //where is ex, ey fields? haha! And Hz, lol.
+  //          //          vector<Matrix> field = {ex,ey,hz,ga};
+  //          //          YeeScheme yee(field); // how would i call yee without field?
+  //          //          // i maybe do not create the yee object in main. Ok.
+  //          //          for(n=1; n<nsteps; ++n)
+  //          //            {
+  //          //                      cout<< "*****Iteration step:"<< n<<"******" << endl;
+  //          //              T= T+1;
+  //          //              yee.iterateSolution(T, YeeScheme::TE_MODE);
+  //          //              //field[0].PrintToFile(T);
+  //          //            }
+  //        } //end te-mode case
+  //
+  //
+  //      case SHITS: //use TE-field..
+  //        {
+  //          Matrix ga(IE,JE),dz(IE,JE),ez(IE,JE) ;
+  //          Matrix hx(IE,JE),hy(IE,JE);
+  //          ga.makeIdentity();
+  //          int dSize = IE;
+  //          //where is ex, ey fields? haha! And Hz, lol.
+  //          vector<Matrix> field = {dz,hx,hy,ez,ga};
+  //          //  vector<Matrix> Efield = {ex,ey,ez};
+  //          YeeScheme yee(field);
+  //
+  //          //create vector to store all the instantiations of yeeScheme..
+  //          //this works:
+  //          /*
+  //             +------+-------------------------------------+-----+
+  //             |  [1] |              [8]                    | [7] |
+  //             |      |                                     |     |
+  //             +------+-------------------------------------+-----+
+  //             |      |                                     |     |
+  //             |      |                                     |     |
+  //             |      |                                     |     |
+  //             |      |                                     |     |
+  //             |      |                                     |     |
+  //             |      |          l'intérieur                |     |
+  //             |  [2] |              [0]                    | [6] |
+  //             |      |                                     |     |
+  //             |      |                                     |     |
+  //             |      |                                     |     |
+  //             |      |                                     |     |
+  //             |      |                                     |     |
+  //             |      |                                     |     |
+  //             |      |                                     |     |
+  //             +------+-------------------------------------+-----+
+  //             |  [3] |             [4]                     | [5] |
+  //             |      |                                     |     |
+  //             +------+-------------------------------------+-----+ */
+  //
+  //          // vector<Matrix> 
+  //          //
+  //          //
+  //          //
+  //          //
+  //
+  //          vector<YeeScheme> myShit(8, YeeScheme(field));
+  //          myShit[0].iterateSolution(T, YeeScheme::TM_MODE);
+  //          //myShit[0].Print();
+  //          field[0].Print();
+  //          //this also works, does not specify how many shits.
+  //          //vector<YeeScheme> myShit(YeeScheme(field));
+  //          //myShit[0].iterateSolution(T, YeeScheme::TM_MODE);
+  //          //ok, objects created, now do the folowing..:
+  //          // call iterateSolution! <- works??
+  //
+  //          for(n=1; n<nsteps; ++n)
+  //            {
+  //                      cout<< "*****Iteration step:"<< n<<"******" << endl;
+  //              T= T+1;
+  //              yee.iterateSolution(T, YeeScheme::TM_MODE);
+  //              //field[0].PrintToFile(T);
+  //            }
+  //        } //end tm-mode case
+  //
+  //      }
 return 0;
 }
 //eof
